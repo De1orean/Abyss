c8f2f1a5488f047a207701832441ef56f5000895
De1orean <39382704+De1orean@users.noreply.github.com>
2020-06-19 23:12:59 +0300

    Initial commit

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+
+    Animator animator;
+
+    float vertical;
+    float horizontal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+
+       // if (vertical == 0)
+          //  animator.SetBool("Run", false);
+
+        if (vertical >= 0.1 && horizontal == 0) 
+            animator.SetBool("Run", true);
+        else
+            animator.SetBool("Run", false);
+
+        //if (vertical >= 0.1 && horizontal >= 0.1)
+        //    animator.SetBool("RunRightForward", true);
+        //else
+        //    animator.SetBool("RunRightForward", false);
+
+        //if (vertical >= 0.1 && horizontal <= -0.1)
+        //    animator.SetBool("RunLeftForward", true);
+        //else
+        //    animator.SetBool("RunLeftForward", false);
+    }
+}
---
+++ Assets/Scripts/ElevatorController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorController : MonoBehaviour
+{
+    [SerializeField] GameObject elevator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        for(int i = 0;i<10000;i++)
+        { 
+        
+        }
+        if(elevator.transform.position.y < 114.23f)
+        elevator.transform.position = new Vector3(elevator.transform.position.x, elevator.transform.position.y + 0.1f, elevator.transform.position.z);
+    }
+}
